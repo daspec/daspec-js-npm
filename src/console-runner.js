6f55e80ec525b2d50a9a78802c4099190ce71b48
Gojko Adzic <gojko@gojko.com>
2015-08-06 18:11:39 +0200

    naive console runner - needs a lot more work

--- src/console-runner.js
+++ src/console-runner.js
@@ -1,22 +1,50 @@
-/*global module, console, require */
+/*global module, require */
 
 var glob = require('glob'),
-	outputPath = require('./output-path');
+	outputPath = require('./output-path'),
+	DaSpec = require('daspec-core'),
+	vm = require('vm'),
+	fs = require('fs'),
+	fsOptions = {encoding: 'utf8'}; /* TODO config encoding? */
 module.exports = function ConsoleRunner(config) {
 	'use strict';
 	var self = this,
-		outputDir = config['output-dir'];
+		outputDir = config['output-dir'],
+		checkConfig = function () {
+			if (!outputDir) {
+				throw new Error('Output directory is not defined');
+			}
+			if (!config.specs || config.specs.length === 0) {
+				throw new Error('No specs defined for this run.');
+			}
+			if (!config.steps || config.steps.length === 0) {
+				throw new Error('No steps defined for this run.');
+			}
+		},
+		checkFiles = function (files) {
+			if (files.steps.length === 0) {
+				throw new Error('No step files match the list provided:', config.steps);
+			}
+			if (files.specs.length === 0) {
+				throw new Error('No spec files match the list provided:', config.specs);
+			}
+		},
+		toScript = function (sourcePath) {
+			var source = fs.readFileSync(sourcePath, fsOptions);
+			return new vm.Script(source, { filename: sourcePath });
+		};
 	self.run = function () {
-		if (!outputDir) {
-			throw new Error('Output directory is not defined');
-		}
-		var files = { specs: [], steps: [], sources: []};
-		if (!config.specs || config.specs.length === 0) {
-			throw new Error('No specs defined for this run.');
-		}
-		if (!config.steps || config.steps.length === 0) {
-			throw new Error('No steps defined for this run.');
-		}
+		var files = { specs: [], steps: [], sources: []},
+			sourceScripts,
+			stepScripts,
+			defineSteps = function (specContext) {
+				var nodeContext = vm.createContext(specContext);
+				sourceScripts.concat(stepScripts).forEach(function (script) {
+					script.runInContext(nodeContext);
+				});
+			};
+
+		checkConfig();
 		Object.keys(files).forEach(function (key) {
 			if (config[key]) {
 				config[key].forEach(function (pattern) {
@@ -24,14 +52,16 @@ module.exports = function ConsoleRunner(config) {
 				});
 			}
 		});
-		if (files.steps.length === 0) {
-			throw new Error('No step files match the list provided:', config.steps);
-		}
-		if (files.specs.length === 0) {
-			throw new Error('No spec files match the list provided:', config.specs);
-		}
-		files.specs.forEach(function (file) {
-			console.log(file, '=>', outputPath(file, outputDir));
+		checkFiles(files);
+		sourceScripts =	files.sources.map(toScript);
+		stepScripts = files.steps.map(toScript);
+
+
+		files.specs.forEach(function (specFile) {
+			var runner = new DaSpec.Runner(defineSteps),
+				source = fs.readFileSync(specFile, fsOptions),
+				result = runner.example(source);
+			fs.writeFileSync(outputPath(specFile), result, fsOptions);
 		});
 	};
 };
