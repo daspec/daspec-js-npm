e0cb357923495d9094ef78ab38732452f2d0cb95
Gojko Adzic <gojko@gojko.com>
2015-08-06 19:00:41 +0200

    initial run, but poluting context

--- src/console-runner.js
+++ src/console-runner.js
@@ -1,4 +1,4 @@
-/*global module, require */
+/*global module, require, console, global */
 
 var glob = require('glob'),
 	outputPath = require('./output-path'),
@@ -12,35 +12,35 @@ module.exports = function ConsoleRunner(config) {
 		outputDir = config['output-dir'],
 		checkConfig = function () {
 			if (!outputDir) {
-				throw new Error('Output directory is not defined');
+				throw 'Output directory is not defined -- re-run with --help to see config arguments';
 			}
 			if (!config.specs || config.specs.length === 0) {
-				throw new Error('No specs defined for this run.');
+				throw 'No specs defined for this run -- re-run with --help to see config arguments';
 			}
 			if (!config.steps || config.steps.length === 0) {
-				throw new Error('No steps defined for this run.');
+				throw 'No steps defined for this run  -- re-run with --help to see config arguments';
 			}
 		},
 		checkFiles = function (files) {
 			if (files.steps.length === 0) {
-				throw new Error('No step files match the list provided:', config.steps);
+				throw 'No step files match the list provided:' + config.steps;
 			}
 			if (files.specs.length === 0) {
-				throw new Error('No spec files match the list provided:', config.specs);
+				throw 'No spec files match the list provided:' + config.specs;
 			}
 		},
 		toScript = function (sourcePath) {
 			var source = fs.readFileSync(sourcePath, fsOptions);
-			return new vm.Script(source, { filename: sourcePath });
+			return new vm.Script(source, sourcePath);
 		};
 	self.run = function () {
 		var files = { specs: [], steps: [], sources: []},
 			sourceScripts,
 			stepScripts,
 			defineSteps = function (specContext) {
-				var nodeContext = vm.createContext(specContext);
+				global.defineStep = specContext.defineStep;
 				sourceScripts.concat(stepScripts).forEach(function (script) {
-					script.runInContext(nodeContext);
+					script.runInThisContext();
 				});
 			};
 
@@ -58,10 +58,17 @@ module.exports = function ConsoleRunner(config) {
 
 
 		files.specs.forEach(function (specFile) {
+			console.log('running', specFile);
 			var runner = new DaSpec.Runner(defineSteps),
-				source = fs.readFileSync(specFile, fsOptions),
-				result = runner.example(source);
-			fs.writeFileSync(outputPath(specFile), result, fsOptions);
+				source,
+				result,
+				outputFile = outputPath(specFile, outputDir);
+			source = fs.readFileSync(specFile, fsOptions);
+			result = runner.example(source);
+			console.log('... done, writing', outputFile);
+			/*
+			fs.writeFileSync(outputFile, result, fsOptions);
+			*/
 		});
 	};
 };
